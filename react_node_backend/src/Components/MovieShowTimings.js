3fc8a2b83f6aa5b08f384d23bb36e8eb63469869
Vajid Kagdi <vajid9@gmail.com>
2018-04-29 16:56:13 -0700

    Show Timing Day Bug Fix

--- react_node_backend/src/Components/MovieShowTimings.js
+++ react_node_backend/src/Components/MovieShowTimings.js
@@ -236,7 +236,7 @@ class Layout extends Component {
         let today = new Date();
         let days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
         let month = ['Jan', 'Fab', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Aug', 'Nov', 'Dec']
-        let weekday = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+        let weekday = ['Sun','Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
         let otherNodes = days.map((item) => {
             let otherDay = new Date;
             otherDay.setDate(today.getDate() + item);
